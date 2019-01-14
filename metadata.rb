6fce42a15155ed4205282258a8e61e9f114434a1
Stephen Hoekstra <shoekstra@schubergphilis.com>
2019-01-14 10:55:43 +0100

    Release 0.1.1

--- metadata.rb
+++ metadata.rb
@@ -2,12 +2,12 @@ name             'crl_copy'
 maintainer       'Schuberg Philis'
 maintainer_email 'cookbooks@schubergphilis.com'
 license          'Apache-2.0'
-version          '0.1.0'
 description      'Installs and configures the CRL Copy PowerShell script from Script Center'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 issues_url       'https://github.com/SBP-Cookbooks/crl_copy/issues'
 source_url       'https://github.com/SBP-Cookbooks/crl_copy'
 chef_version     '>= 14'
+version          '0.1.1'
 
 supports 'windows'
 
