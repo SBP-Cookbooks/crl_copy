f76e02351619cd14352689ee1288ceb6264ef508
Stephen Hoekstra <shoekstra@schubergphilis.com>
2019-01-14 10:35:41 +0100

    Fix spacing
    Signed-off-by: Stephen Hoekstra <shoekstra@schubergphilis.com>

--- attributes/default.rb
+++ attributes/default.rb
@@ -18,25 +18,21 @@
 
 # CRL Copy default attributes, these can be overridden when using the LWRP.
 
-default['crl_copy']['eventvwr']['event_source']      = 'CRL Copy Process'
-default['crl_copy']['eventvwr']['event_id']          = 5000
-default['crl_copy']['eventvwr']['event_high']        = 1
-default['crl_copy']['eventvwr']['event_warning']     = 2
-default['crl_copy']['eventvwr']['event_information'] = 4
+default['crl_copy']['eventvwr']['event_source'] = 'CRL Copy Process'
+default['crl_copy']['eventvwr']['event_id'] = 5000
 
+default['crl_copy']['smtp']['send_mail'] = false
+default['crl_copy']['smtp']['server'] = nil
+default['crl_copy']['smtp']['from'] = nil
+default['crl_copy']['smtp']['to'] = nil
+default['crl_copy']['smtp']['published_notify'] = false
+default['crl_copy']['smtp']['title'] = 'CRL Copy Process Results'
+default['crl_copy']['smtp']['threshold'] = 2
 
-default['crl_copy']['smtp']['send_mail']             = false
-default['crl_copy']['smtp']['server']                = nil
-default['crl_copy']['smtp']['from']                  = nil
-default['crl_copy']['smtp']['to']                    = nil
-default['crl_copy']['smtp']['published_notify']      = false
-default['crl_copy']['smtp']['title']                 = 'CRL Copy Process Results'
-default['crl_copy']['smtp']['threshold']             = 2
+default['crl_copy']['warnings']['threshold'] = 5
+default['crl_copy']['warnings']['threshold_unit'] = 'Hours'
 
-default['crl_copy']['warnings']['threshold']         = 5
-default['crl_copy']['warnings']['threshold_unit']    = 'Hours'
-
-default['crl_copy']['windows_task']['frequency']          = 'Minute'
+default['crl_copy']['windows_task']['frequency'] = 'Minute'
 default['crl_copy']['windows_task']['frequency_modifier'] = '30'
-default['crl_copy']['windows_task']['password']           = nil
-default['crl_copy']['windows_task']['user']               = 'SYSTEM'
+default['crl_copy']['windows_task']['password'] = nil
+default['crl_copy']['windows_task']['user'] = 'SYSTEM'
